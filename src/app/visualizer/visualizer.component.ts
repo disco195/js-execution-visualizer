860fdb8efb2015d843f20c0f8e3de4146f2f62d7
saichaitanyareddy <saichaitanya338@gmail.com>
2020-08-22 18:43:23 +0530

    Add visualizer component

--- src/app/visualizer/visualizer.component.ts
+++ src/app/visualizer/visualizer.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ROW_TITLES } from './visualizer.constants';
 @Component({
   selector: 'app-visualizer',
   templateUrl: './visualizer.component.html',
   styleUrls: ['./visualizer.component.scss']
 })
 export class VisualizerComponent implements OnInit {
-
+  public displayBlockNumber = 0;
+  public rowTitles = ROW_TITLES;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  incrementDisplayBlockNumber(): void {
+    this.displayBlockNumber++;
+  }
+
+  decrementDisplayBlockNumber(): void {
+    this.displayBlockNumber--;
   }
 
 }
---
+++ src/app/visualizer/visualizer.constants.ts
@@ -0,0 +1,7 @@
+export const ROW_TITLES = {
+    taskQueue: 'TASK QUEUE',
+    microTaskQueue: 'MICRO TASK QUEUE',
+    webApis: 'WEB API',
+    callStack: 'CALL STACK',
+    console: 'LOG'
+}
